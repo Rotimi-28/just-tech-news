f8d40f3b3d77866a347b71013dd1ea6a76746452
Rotimi.E <matanmearoja@yahoo.com>
2022-04-20 13:40:16 -0700

    commit the module file

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+const apiRoutes = require(".api");
+
+router.user("./api", apiRoutes);
+
+
+router.user((req, res) => {
+    res.status(404).end();
+});
+
+
+
+module.exports = router;
